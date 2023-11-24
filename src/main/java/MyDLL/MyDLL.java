7b6590d73e4b0aee7380e21453584cecabbb5a65
PatonFTW <patonFTWADED@gmail.com>
2023-11-24 11:20:58 -0700

    Forgot documentation for some files/
    .

--- src/main/java/MyDLL/MyDLL.java
+++ src/main/java/MyDLL/MyDLL.java
@@ -3,29 +3,52 @@ package MyDLL;
 /**
  * Doubly Linked List class.
  *
+ * This class represents a doubly linked list data structure, where each node
+ * contains an element of type E. The list maintains references to the head and
+ * tail nodes, allowing efficient addition and removal of elements from both
+ * ends. The size of the list is also tracked for convenience.
+ *
  * @param <E> the type of elements in the list.
  * 
- * Author Patryk
+ * @author Patryk
  */
 public class MyDLL<E> {
     private MyDLLNode<E> head;
     private MyDLLNode<E> tail;
     private int size;
 
+    /**
+     * Constructs an empty doubly linked list.
+     */
     public MyDLL() {
         this.head = null;
         this.tail = null;
         this.size = 0;
     }
 
+    /**
+     * Checks if the doubly linked list is empty.
+     *
+     * @return true if the list is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Returns the current size of the doubly linked list.
+     *
+     * @return the size of the list.
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Adds an element to the front of the doubly linked list.
+     *
+     * @param element the element to be added to the front.
+     */
     public void addToFront(E element) {
         MyDLLNode<E> newNode = new MyDLLNode<>(element);
         if (isEmpty()) {
@@ -38,6 +61,11 @@ public class MyDLL<E> {
         size++;
     }
 
+    /**
+     * Adds an element to the end of the doubly linked list.
+     *
+     * @param element the element to be added to the end.
+     */
     public void addToEnd(E element) {
         MyDLLNode<E> newNode = new MyDLLNode<>(element);
         if (isEmpty()) {
@@ -50,6 +78,11 @@ public class MyDLL<E> {
         size++;
     }
 
+    /**
+     * Removes and returns the element from the front of the doubly linked list.
+     *
+     * @return the element removed from the front, or null if the list is empty.
+     */
     public E removeFromFront() {
         if (isEmpty()) {
             return null;
@@ -68,6 +101,11 @@ public class MyDLL<E> {
         return removedElement;
     }
 
+    /**
+     * Removes and returns the element from the end of the doubly linked list.
+     *
+     * @return the element removed from the end, or null if the list is empty.
+     */
     public E removeFromEnd() {
         if (isEmpty()) {
             return null;
--- src/main/java/MyDLL/MyDLLNode.java
+++ src/main/java/MyDLL/MyDLLNode.java
@@ -3,37 +3,71 @@ package MyDLL;
 /**
  * Node class for a Doubly Linked List.
  *
+ * This class represents a node in a doubly linked list, containing an element
+ * of type E. Each node maintains references to the previous and next nodes,
+ * allowing traversal in both directions.
+ *
  * @param <E> the type of elements in the node.
  * 
- * Author Patryk
+ * @author Patryk
  */
 public class MyDLLNode<E> {
     private E element;
     private MyDLLNode<E> previous;
     private MyDLLNode<E> next;
 
+    /**
+     * Constructs a node with the specified element.
+     *
+     * @param element the element to be stored in the node.
+     */
     public MyDLLNode(E element) {
         this.element = element;
         this.previous = null;
         this.next = null;
     }
 
+    /**
+     * Returns the element stored in the node.
+     *
+     * @return the element in the node.
+     */
     public E getElement() {
         return element;
     }
 
+    /**
+     * Returns the previous node in the doubly linked list.
+     *
+     * @return the previous node, or null if no previous node exists.
+     */
     public MyDLLNode<E> getPrevious() {
         return previous;
     }
 
+    /**
+     * Sets the previous node for this node.
+     *
+     * @param previous the node to be set as the previous node.
+     */
     public void setPrevious(MyDLLNode<E> previous) {
         this.previous = previous;
     }
 
+    /**
+     * Returns the next node in the doubly linked list.
+     *
+     * @return the next node, or null if no next node exists.
+     */
     public MyDLLNode<E> getNext() {
         return next;
     }
 
+    /**
+     * Sets the next node for this node.
+     *
+     * @param next the node to be set as the next node.
+     */
     public void setNext(MyDLLNode<E> next) {
         this.next = next;
     }
